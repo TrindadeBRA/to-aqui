5f9a39f56e3944d128ff8520c6f6baaef05ce026
Lucas Trindade <trindadebra@gmail.com>
2024-10-27 20:20:46 -0300

    feat: Menu mobile logo added

--- src/app/app/_components/mobile-nav.tsx
+++ src/app/app/_components/mobile-nav.tsx
@@ -5,6 +5,7 @@ import { Button } from '@/components/ui/button'
 import { HamburgerMenuIcon } from '@radix-ui/react-icons'
 import { MainSidebar } from './main-sidebar'
 import { Session } from 'next-auth'
+import { Logo } from '@/components/logo'
 
 type MobileNavProps = {
   user: Session['user']
@@ -12,15 +13,20 @@ type MobileNavProps = {
 
 export function MobileNav({ user }: MobileNavProps) {
   return (
-    <Sheet>
-      <SheetTrigger asChild>
-        <Button variant="ghost" className="md:hidden">
-          <HamburgerMenuIcon className="h-5 w-5" />
-        </Button>
-      </SheetTrigger>
-      <SheetContent side="left" className="p-0 w-72">
-        <MainSidebar user={user} />
-      </SheetContent>
-    </Sheet>
+    <div className="flex items-center gap-x-2 md:hidden">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="ghost">
+            <HamburgerMenuIcon className="h-5 w-5" />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left" className="p-0 w-72">
+          <MainSidebar user={user} />
+        </SheetContent>
+      </Sheet>
+      <div className="">
+        <Logo />
+      </div>
+    </div>
   )
 }
