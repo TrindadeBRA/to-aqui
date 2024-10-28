0a7f4c2463164fdd455ece164a2f9fc882961b66
Lucas Trindade <trindadebra@gmail.com>
2024-10-28 16:52:50 -0300

    back

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -33,68 +33,10 @@ export const {
   ],
   events: {
     createUser: async (message) => {
-      try {
-        if (!message.user.email) {
-          console.error('Email não fornecido durante a criação do usuário')
-          throw new Error('Email é obrigatório')
-        }
-
-        console.log('Dados completos do usuário:', message.user)
-        console.log('Iniciando criação do cliente no Stripe:', {
-          email: message.user.email,
-          name: message.user.name || 'Usuário Anônimo', // fallback para quando name for null
-        })
-
-        await createStripeCustomer({
-          name: message.user.name || 'Usuário Anônimo',
-          email: message.user.email as string,
-        })
-
-        console.log('Cliente criado com sucesso no Stripe')
-        console.log('Status da sessão após criação:', await auth())
-      } catch (error) {
-        console.error('Erro detalhado ao criar cliente:', {
-          error,
-          userData: message.user,
-          timestamp: new Date().toISOString(),
-        })
-        throw error
-      }
-    },
-  },
-  callbacks: {
-    signIn: async ({ user, account, profile }) => {
-      console.log('Tentativa de login:', { user, account, profile })
-      return true
-    },
-    session: async ({ session, user }) => {
-      // Garantir que todos os dados do usuário estejam na sessão
-      return {
-        ...session,
-        user: {
-          ...session.user,
-          ...user,
-        },
-      }
-    },
-    redirect: async ({ url, baseUrl }) => {
-      console.log('Redirecionamento - URL:', url)
-      console.log('Redirecionamento - Base URL:', baseUrl)
-
-      // Se o usuário acabou de se registrar/logar, force o redirecionamento para /app
-      if (url.includes('/api/auth/callback')) {
-        console.log('Redirecionando para /app após autenticação')
-        return `${baseUrl}/app`
-      }
-
-      // Para outros casos, mantenha o comportamento padrão
-      if (url.startsWith(baseUrl)) {
-        console.log('Mantendo URL original:', url)
-        return url
-      }
-
-      console.log('Redirecionando para base URL:', baseUrl)
-      return baseUrl
+      await createStripeCustomer({
+        name: message.user.name as string,
+        email: message.user.email as string,
+      })
     },
   },
 })
